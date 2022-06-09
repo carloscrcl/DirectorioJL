a6414cb1404fb08b7a5e135f9c69761ea61d440a
carloscrcl <carloscrcl@gmail.com>
2022-06-09 18:36:06 -0500

    SE REALIZA LA ESTRUCTURA PARA MOSTRAR LOS DISTRITOS

--- app.js
+++ app.js
@@ -103,37 +103,131 @@ let peticion = fetch("directoriojl.json")
     document.body.appendChild(contenedor);
 })
 
+// -------- DISTRITOS ---------------------
+
 let segudaPeticion = fetch("directoriojds.json")
     .then(res2 => res2.json())
     .then(res => {
+      for (let i = 1; i <= 20; i++) {
         
-
-        let obj = res.jd1;
+        
+        let obj = res["jd"+i];
         console.log(obj);
+        let calle = document.createElement("p");
+        let col = document.createElement("p");
+        let referencia = document.createElement("p");
+        let cp = document.createElement("p");
+        let mpio = document.createElement("p");
+
         for (const vocal in obj) {
-            let gridCelda = document.createElement("div");
-            let gridBody = document.createElement("div");
             
-            let gridImage = document.createElement("div");
-            let img = document.createElement("img");
-            
-            let gridTextos = document.createElement("div");
-            let nombre = document.createElement("p");
-            let appaterno = document.createElement("p");
-            let apmaterno = document.createElement("p");
-            let email = document.createElement("p");
-            
-            let rutaImagen = obj[vocal].foto;
-            img.src = rutaImagen;
-            
-            gridImage.appendChild(img);
-            
-
-
-
             if(vocal == 6 ){
-                console.log("estoy en direccion");
+              let gridCelda = document.createElement("div");
+              let gridDireccion = document.createElement("div");
+              let direccion = obj[vocal].direccion;
+                
+                calle.textContent = direccion[0].calle +" Num. "+ direccion[0].numero;
+                col.textContent = direccion[0].colonia;
+                referencia.textContent = direccion[0].referencia;
+                cp.textContent = direccion[0].cp;
+                mpio.textContent = direccion[0].mpio;
+              
+                
+                gridDireccion.appendChild(calle);
+                gridDireccion.appendChild(col);
+                gridDireccion.appendChild(referencia);
+                gridDireccion.appendChild(cp);
+                gridDireccion.appendChild(mpio);
+
+                gridCelda.appendChild(gridDireccion);
+                // console.log(gridCelda);
+                fragmento.appendChild(gridCelda);
+                contenedorJd.appendChild(fragmento);
+                // console.log(contenedorJd);
+                // break;
+            }else if (vocal == 7) {
+               let telefonos = obj[vocal].telefonos[0];
+               let gridCelda = document.createElement("div");
+               let tels = document.createElement("div");
+               console.log( telefonos);
+               for (const tel in telefonos) {
+                  let p = document.createElement("p");
+                  let span = document.createElement("span");
+                  span.textContent = telefonos[tel];
+                  p.appendChild(span);
+                  tels.appendChild(p);
+                }
+                
+                gridCelda.appendChild(tels);
+                fragmento.appendChild(gridCelda);
+                contenedorJd.appendChild(fragmento);
+              
+              
+            }
+            
+            
+            else{
+
+                
+                let gridC = document.createElement("div");
+                let adscripcion = document.createElement("div");
+                let ads = obj["junta"][vocal].junta;
+                let p =  document.createElement("p");
+                p.textContent = ads.id +" " + ads.ubiciacion; 
+                
+                adscripcion.appendChild(p);
+
+                gridC.appendChild(adscripcion);
+                fragmento.appendChild(gridC);
+                contenedorJd.appendChild(fragmento);
+                
+                                  
+
+                let gridCelda = document.createElement("div");
+                let gridBody = document.createElement("div");
+                
+                let gridImage = document.createElement("div");
+                let img = document.createElement("img");
+                
+                let gridTextos = document.createElement("div");
+                let nombre = document.createElement("p");
+                let appaterno = document.createElement("p");
+                let apmaterno = document.createElement("p");
+                let email = document.createElement("p");
+                let link = document.createElement("a");
+                
+                let rutaImagen = obj[vocal].foto;
+                img.src = rutaImagen;
+                gridImage.appendChild(img);
+                
+                nombre.textContent = obj[vocal].nombres;
+                appaterno.textContent = obj[vocal].appaterno;
+                apmaterno.textContent = obj[vocal].apmaterno;
+                email.textContent = obj[vocal].email;
+                link.appendChild(email);
+    
+                            
+                gridTextos.appendChild(nombre);
+                gridTextos.appendChild(appaterno);
+                gridTextos.appendChild(apmaterno);
+                gridTextos.appendChild(link);
+                
+                gridBody.appendChild(gridImage);
+                gridBody.appendChild(gridTextos);
+                gridCelda.appendChild(gridBody);
+                
+                
+                
+                
+                
+                
+                fragmento.appendChild(gridCelda);
+                contenedorJd.appendChild(fragmento);
+                
             }
         }  
+      }
+        document.body.appendChild(contenedorJd);
+       
 
     })
